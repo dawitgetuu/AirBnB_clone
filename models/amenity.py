3eed4ea6e19015d456b3968d9448004448f94bd3
dawitgetuu <archfun3@gmail.com>
2024-02-13 09:17:55 -0800

    0x00. AirBnB clone - The console

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
+"""This module creates a Amenity class"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
+    """Class for managing amenity objects"""
 
-    Attributes:
-        name (str): The name of the amenity.
-    """
-
-    name = ""
--- models/place.py
+++ models/place.py
@@ -30,4 +30,3 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = []
